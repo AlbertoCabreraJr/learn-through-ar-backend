f5234cd3b7a08b1fb87c809a6f5fd699e93964f2
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2023-04-04 15:42:27 +0800

    Fix

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,14 +1,23 @@
 import { useNavigate } from 'react-router-dom'
 import logo from '../assets/logo.svg'
+import { useAuthContext } from '../context/AuthContext'
 
 const ErrorPage = () => {
   const navigate = useNavigate()
+  const { signOut } = useAuthContext()
+
   return (
     <div className='error-page'>
       <img className='logo' src={logo} alt='Learn through AR logo' />
       <div className='text'>Oops, something went wrong!</div>
-      <div className='go-back' onClick={() => navigate('/sign-in')}>
-        Go back to Home
+      <div
+        className='go-back'
+        onClick={async () => {
+          signOut && signOut()
+          navigate('/sign-in', { replace: true })
+        }}
+      >
+        Go to Sign-in page
       </div>
     </div>
   )
--- src/pages/TopicPage/index.tsx
+++ src/pages/TopicPage/index.tsx
@@ -52,24 +52,27 @@ const TopicPage = () => {
 
   const handleFinishTopic = async () => {
     // No need to update if topic is already finished
-    if (!topic.finished) {
-      await updateTopic({ topicId: topicId!, body: { finished: true } })
-      await updateModule({ moduleId: moduleId!, body: { progress: module?.progress! + 1 } })
-
-      const currentFinishedTopics = course?.finishedTopics ? course.finishedTopics : []
-      const newFinishedTopics = [...currentFinishedTopics, topicId as string]
-
-      const currentTopicIndex = module?.topics?.findIndex((topic) => topicId === topic._id)
-      const newCurrentTopic = module?.topics[(currentTopicIndex as number) + 1]
-
-      await updateCourse({
-        courseId: courseId!,
-        body: {
-          finishedTopics: newFinishedTopics ? newFinishedTopics : [],
-          currentTopic: newCurrentTopic?._id
-        }
-      })
+    if (topic.finished) {
+      navigate(`/course/${courseId}/module/${moduleId}`, { replace: true })
+      return
     }
+
+    await updateTopic({ topicId: topicId!, body: { finished: true } })
+    await updateModule({ moduleId: moduleId!, body: { progress: module?.progress! + 1 } })
+
+    const currentFinishedTopics = course?.finishedTopics ? course.finishedTopics : []
+    const newFinishedTopics = [...currentFinishedTopics, topicId as string]
+
+    const currentTopicIndex = module?.topics?.findIndex((topic) => topicId === topic._id)
+    const newCurrentTopic = module?.topics[(currentTopicIndex as number) + 1]
+
+    await updateCourse({
+      courseId: courseId!,
+      body: {
+        finishedTopics: newFinishedTopics ? newFinishedTopics : [],
+        currentTopic: newCurrentTopic?._id
+      }
+    })
 
     navigate(`/course/${courseId}/module/${moduleId}`, { replace: true })
   }
