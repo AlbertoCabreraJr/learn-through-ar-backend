e7c96e8cfaf92da112b8d2d00f35255b4ee1ca7a
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2023-04-04 16:27:37 +0800

    Fix padding

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -8,7 +8,7 @@ const Loader = () => {
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '100vh'
+        height: '80vh'
       }}
     >
       <div className='spinner'>
