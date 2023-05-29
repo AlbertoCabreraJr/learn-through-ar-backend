50b88fe7e58cd96a97c7138a774258a72a222137
Alberto A. Cabrera Jr <alberto@Alberto-Mac.local>
2023-05-29 22:52:54 +0800

    update

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -9,7 +9,7 @@ const ErrorPage = () => {
   return (
     <div className='error-page'>
       <img className='logo' src={logo} alt='Learn through AR logo' />
-      <div className='text'>Oops, your 1 hour is session expired!</div>
+      <div className='text'>Oops, your session is expired!</div>
       <div
         className='go-back'
         onClick={async () => {
